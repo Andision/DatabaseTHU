24fc7024f07d57fd4f50c8d70c2b0fb36b73af3c
Andision <48558940+Andision@users.noreply.github.com>
2022-09-29 23:28:19 +0800

    Add route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Home/Nav0';
 import Footer from './Home/Footer1';
 import Home from './Home';
 import Page from './Page2';
+import Detail from './Detail';
 
 import {
   Nav00DataSource,
@@ -36,7 +37,8 @@ class App extends Component {
         <div>
           <Header dataSource={Nav00DataSource} isMobile={this.state.isMobile} />
           <Route exact path="/" component={Home} />
-          <Route path="/abc" component={Page} />
+          <Route path="/search" component={Page} />
+          <Route path="/detail" component={Detail} />
           <Footer dataSource={Footer10DataSource} isMobile={this.state.isMobile} />
         </div>
       </Router>
