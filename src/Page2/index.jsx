38c05ae257c9a246e71613c645eaafa9bfa2e1ac
Andision <48558940+Andision@users.noreply.github.com>
2022-09-29 23:27:40 +0800

    Add Search

--- src/Page2/index.jsx
+++ src/Page2/index.jsx
@@ -1,78 +1,81 @@
-/* eslint no-undef: 0 */
-/* eslint arrow-parens: 0 */
 import React from 'react';
-import { enquireScreen } from 'enquire-js';
+import { Row, List, Avatar, Input } from 'antd';
 
-import Content0 from './Content0';
-import Content7 from './Content7';
+import './index.css'
 
-import {
-  Content00DataSource,
-  Content70DataSource,
-} from './data.source.js';
+const { Search } = Input;
 
-import './less/antMotionStyle.less';
-
-let isMobile;
-enquireScreen((b) => {
-  isMobile = b;
-});
-
-const location = window.location;
+const data = Array.from({ length: 23 }).map((_, i) => ({
+  href: 'https://ant.design',
+  title: `ant design part ${i}`,
+  avatar: 'https://joeschmoe.io/api/v1/random',
+  description:
+    'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+  content:
+    'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+}));
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMobile,
-      show: !location.port, // 如果不是 dva 2.0 请删除
-    };
-  }
-
-  componentDidMount() {
-    // 适配手机屏幕;
-    enquireScreen((b) => {
-      this.setState({ isMobile: !!b });
-    });
-    // dva 2.0 样式在组件渲染之后动态加载，导致滚动组件不生效；线上不影响；
-    /* 如果不是 dva 2.0 请删除 start */
-    if (location.port) {
-      // 样式 build 时间在 200-300ms 之间;
-      setTimeout(() => {
-        this.setState({
-          show: true,
-        });
-      }, 500);
-    }
-    /* 如果不是 dva 2.0 请删除 end */
-  }
-
   render() {
-    const children = [
-      <Content0
-        id="Content0_0"
-        key="Content0_0"
-        dataSource={Content00DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      // <Content7
-      //   id="Content7_0"
-      //   key="Content7_0"
-      //   dataSource={Content70DataSource}
-      //   isMobile={this.state.isMobile}
-      // />,
-    ];
+    // const onSearch = () => {
+    // };
     return (
-      <div
-        className="templates-wrapper"
-        ref={(d) => {
-          this.dom = d;
-        }}
-      >
-        {/* 如果不是 dva 2.0 替换成 {children} start */}
-        {this.state.show && children}
-        {/* 如果不是 dva 2.0 替换成 {children} end */}
+      <div className='search-bg'>
+        <Row className='search-title'>
+          <span>
+            数据检索
+          </span>
+        </Row>
+        <Row>
+          <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton size="large" />
+        </Row>
+        <Row className='search-result'>
+          <List
+            itemLayout="vertical"
+            size="large"
+            pagination={{
+              onChange: page => {
+                console.log(page);
+              },
+              pageSize: 5,
+              position: 'bottom',
+              // showQuickJumper:true,
+              // showSizeChanger:true
+            }
+            }
+            dataSource={data}
+            footer={
+              <div>
+                {/* <b>ant design</b> footer part */}
+              </div>
+            }
+            renderItem={item => (
+              <List.Item
+                key={item.title}
+                // actions={[
+                //   <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                //   <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                //   <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                // ]}
+                extra={
+                  <img
+                    width={272}
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                  />
+                }
+              >
+                <List.Item.Meta
+                  avatar={<Avatar src={item.avatar} />}
+                  title={<a href={item.href}>{item.title}</a>}
+                  description={item.description}
+                />
+                {item.content}
+              </List.Item>
+            )}
+          />
+        </Row>
       </div>
-    );
+    )
   }
 }
