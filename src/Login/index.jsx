87cc137eddeb18804c2cdeb4aab754d81a34634c
Andision <48558940+Andision@users.noreply.github.com>
2022-12-16 21:46:38 +0800

    Update Login for antd v4

--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -1,94 +1,98 @@
-import React from 'react';
-import { Form, Icon, Input, Button, Checkbox, Card, message } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import axios from 'axios';
-import querystring from "querystring"
-
-import './index.css'
-
+import { Button, Checkbox, Form, Input, Card, message } from 'antd';
+import React from 'react';
+import "./index.css"
 class FormLogin extends React.Component {
     render() {
-        const { getFieldDecorator } = this.props.form;
-        const handleSubmit = e => {
-            const source_page = querystring.parse(this.props.location.search.slice(1)).source
+        const onFinish = (values) => {
+            // console.log('Received values of form: ', values);
+            axios.post('/hydrologyAPI/login/', { "email": values.username, "password": values.password }).then((response) => {
+                // console.log(response.data.code, response)
+                if (response.status === 200) {
+                    if (response.data.code === 0) {
+                        message.success('登录成功，正在跳转！');
+                        // if (source_page !== undefined) {
+                        //     setTimeout(() => this.props.history.push(source_page), 3000);
+                        // }
+                        // else {
+                        // }
+                        setTimeout(() => this.props.history.push("/search"), 3000);
 
-            e.preventDefault();
-            this.props.form.validateFields((err, values) => {
-                if (!err) {
-                    // console.log('Received values of form: ', values);
-                    // console.log(this)
-                    axios.post('/hydrologyAPI/login/', { "email": values.username, "password": values.password }).then((response) => {
-                        // console.log(response.data.code, response)
-                        if (response.status === 200) {
-                            if (response.data.code === 0) {
-                                message.success('登录成功，正在跳转！');
-                                if (source_page !== undefined) {
-                                    setTimeout(() => this.props.history.push(source_page), 3000);
-                                }
-                                else {
-                                    setTimeout(() => this.props.history.push("/search"), 3000);
-                                }
-
-                            }
-                            else if (response.data.code === -1) {
-                                message.warning('用户名或密码错误！');
-                            }
-                            else {
-                                message.error('登录失败，请稍后重试！');
-                            }
-                        }
-                        else {
-                            message.error('网络错误，请稍后重试！');
-                        }
-
-                    });
+                    }
+                    else if (response.data.code === -1) {
+                        message.warning('用户名或密码错误！');
+                    }
+                    else {
+                        message.error('登录失败，请稍后重试！');
+                    }
                 }
+                else {
+                    message.error('网络错误，请稍后重试！');
+                }
+
             });
         };
-
         return (
             <div className='login-bg'>
+
                 <Card className='login-card' headStyle={{ textAlign: "center" }} title="登录">
 
-                    <Form onSubmit={handleSubmit} className="login-form">
-                        <Form.Item>
-                            {getFieldDecorator('username', {
-                                rules: [{ required: true, message: '请输入用户名！' }],
-                            })(
-                                <Input
-                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    placeholder="用户名"
-                                />,
-                            )}
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入用户名！',
+                                },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入密码！',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="密码"
+                            />
                         </Form.Item>
                         <Form.Item>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: '请输入密码！' }],
-                            })(
-                                <Input
-                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    type="password"
-                                    placeholder="密码"
-                                />,
-                            )}
-                        </Form.Item>
-                        <Form.Item>
-                            {getFieldDecorator('remember', {
-                                valuePropName: 'checked',
-                                initialValue: false,
-                            })(<Checkbox>记住账号</Checkbox>)}
-                            {/* <a className="login-form-forgot" href="/password">
-                                忘记密码？
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>记住账号</Checkbox>
+                            </Form.Item>
+
+                            {/* <a className="login-form-forgot" href="">
+                                Forgot password
                             </a> */}
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                登录
-                            </Button>
-                            <a href="/register">注册账号</a>
                         </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                            </Button>
+                        </Form.Item>
+                        没有账号？<a href="/register">注册账号</a>
                     </Form>
                 </Card>
             </div>
-        )
+        );
     }
 }
 
-export default Form.create()(FormLogin)
+export default FormLogin;
